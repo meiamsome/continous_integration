96aebdcfac47ff5ff4d31b80da49ce4ac7e5aed7
Oliver <meiamsome@meiamso.me>
2015-10-02 04:20:08 +0100

    Added git module, to start implementing the actual server

---
+++ git/models.py
@@ -0,0 +1,37 @@
+from django.conf import settings
+from django.db import models
+
+AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+REPO_FILE_PATH = getattr(settings, 'REPO_FILE_PATH', './repos/')
+
+
+class Repository(models.Model):
+    """Represents a git repository"""
+    owner = models.ForeignKey(AUTH_USER_MODEL)
+    mainline = models.ForeignKey("Branch", null=True, blank=True, related_name='__not_used')
+
+
+class Commit(models.Model):
+    """Represents a single commit. previous_commit_left is the last commit in this branch, previous_commit_right is the
+    merged commit from a merge commit, if it exists."""
+    repository = models.ForeignKey(Repository)
+    hash = models.CharField(max_length=40)
+    previous_commit_left = models.ForeignKey("Commit", null=True, blank=True, related_name="_next_left_set")
+    previous_commit_right = models.ForeignKey("Commit", null=True, blank=True, related_name="_next_right_set")
+
+    def next_commit_set(self):
+        return Commit.objects.filter(
+            models.Q(previous_commit_left__pk=self.pk) | models.Q(previous_commit_right__pk=self.pk)
+        )
+
+    def is_first_commit(self):
+        return self.previous_commit_left is None
+
+    def is_merge(self):
+        return self.previous_commit_right is not None
+
+
+class Branch(models.Model):
+    """Represents a branch of a git tree, with it's own head"""
+    repository = models.ForeignKey(Repository)
+    head = models.ForeignKey(Commit)
