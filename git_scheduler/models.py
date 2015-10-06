d4d92e16fa0307419a0580bd217d07d0a46d5640
Oliver <meiamsome@meiamso.me>
2015-10-06 13:16:38 +0100

    Added ability for per-branch tasks

--- git_scheduler/models.py
+++ git_scheduler/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.dispatch import receiver
 
-from git.models import Push, Repository
+from git.models import Push, Repository, Branch
 from task_manager.models import Task, ScheduledTask
 
 
 class RegisteredTask(models.Model):
     repository = models.ForeignKey(Repository)
+    branch = models.ForeignKey(Branch, null=True, blank=True)
     task = models.ForeignKey(Task)
     user = models.CharField(max_length=32)
     assign_on_push = models.BooleanField(default=False)
@@ -18,8 +19,9 @@ def build_tasks(sender, instance, created, *args, **kwargs):
         repository = instance.repository
         tasks = RegisteredTask.objects.filter(assign_on_push=True, repository=repository)
         for task in tasks:
-            ScheduledTask(task=task.task, user=task.user, arguments=" ".join((
-                repository.get_name(),
-                sender.branch.ref,
-                instance.after.hash,
-            ))).save()
+            if task.branch is None or task.branch == sender.branch:
+                ScheduledTask(task=task.task, user=task.user, arguments=" ".join((
+                    repository.get_name(),
+                    sender.branch.ref,
+                    instance.after.hash,
+                ))).save()
