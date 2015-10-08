74e386ba7b571448bc7c0152841969af54010387
Oliver <meiamsome@meiamso.me>
2015-10-08 21:36:10 +0100

    Slack integration signals created.

--- slack_messenger/models.py
+++ slack_messenger/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from git.models import Repository
+from slackclient import SlackClient
+
+from git.models import Repository, Branch
 
 
 # Create your models here.
@@ -7,3 +9,23 @@ class SlackBot(models.Model):
     name = models.CharField(max_length=100)
     token = models.CharField(max_length=100)
     repositories = models.ManyToManyField(Repository)
+
+    def get_client(self):
+        if not hasattr(self, '__slack_client'):
+            self.__slack_client = SlackClient(self.token)
+        return self.__slack_client
+
+
+class SlackAlerts(models.Model):
+    bot = models.ForeignKey(SlackBot)
+    repository = models.ForeignKey(Repository)
+    branch = models.ForeignKey(Branch, blank=True)
+    channel = models.CharField(max_length=100)
+
+    def alert(self, message):
+        client = self.bot.get_client()
+        client.api_call('chat.postMessage', {
+            'channel': self.channel,
+            'username': self.bot.name,
+            'text': message,
+        })
---
+++ slack_messenger/signals.py
@@ -0,0 +1,38 @@
+from django.dispatch import receiver
+from django.db.models import Q
+from django.db.models.signals import post_save
+
+from git.models import Push
+from git_scheduler.models import RegisteredTask, TaskToPush
+from task_manager.models import ScheduledTask
+from slack_messenger.models import SlackAlerts
+
+
+@receiver(post_save)
+def handle_task_save(sender, instance, created, *args, **kwargs):
+    if sender == TaskToPush:
+        if created:
+            repository = instance.push.repository
+            branch = instance.push.branch
+            relevant_alerts = SlackAlerts.object.filter(repository=repository, Q(branch=branch) | Q(branch=None))
+            if not relevant_alerts:
+                return
+            message = "Task '%s' assigned to commit %s on %s/%s" %\
+                      (instance.task, instance.push.after, instance.push.repository, instance.push.branch)
+            for alert in relevant_alerts:
+                alert.alert(message)
+
+    if sender == ScheduledTask:
+        if not created:
+            task_to_push = TaskToPush.objects.get(task=instance)
+            repository = task_to_push.push.repository
+            branch = task_to_push.push.branch
+            relevant_alerts = SlackAlerts.object.filter(repository=repository, Q(branch=branch) | Q(branch=None))
+            if not relevant_alerts:
+                return
+            message = "Task '%s' updated for commit %s on %s/%s.\nStatus: %s" %\
+                      (instance, task_to_push.push.after, repository, branch, instance.get_status_display())
+            if instance.status != ScheduledTask.QUEUED:
+                message += "\nOutput:\n%s" % (instance.output, )
+            for alert in relevant_alerts:
+                alert.alert(message)
